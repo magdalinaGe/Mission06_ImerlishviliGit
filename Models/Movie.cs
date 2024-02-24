49ac8e8902e092f9c8985af95c8ba806cdce454a
magdalinaGe <20mimerlishvili@isd221.net>
2024-02-24 01:12:15 -0700

    3

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,14 +10,14 @@ namespace Mission06_Imerlishvili.Models
         public int? MovieId { get; set; }
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; }
         [Range(1888, 2024)]
-        public int? Year { get; set; }
+        public int Year { get; set; }
         public string? Director { get; set; }
         public string? Rating { get; set; }
-        public bool? Edited { get; set; }
+        public bool Edited { get; set; }
         public string? LentTo { get; set; }
-        public bool? CopiedToPlex { get; set; }
+        public bool CopiedToPlex { get; set; }
         public string? Notes { get; set; }
     }
 }
